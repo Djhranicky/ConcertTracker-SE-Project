765902974044a4c8f455c54b6ca2c2700720afbf
David Hranicky <djh30000@gmail.com>
2025-02-02 10:59:07 -0500

    Proof of concept for simple DB add

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"log"
+
 	"github.com/djhranicky/ConcertTracker-SE-Project/cmd/api"
 	"github.com/djhranicky/ConcertTracker-SE-Project/db"
+	"github.com/djhranicky/ConcertTracker-SE-Project/types"
 )
 
 func main() {
 	db, err := db.NewSqliteStorage()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&types.User{})
+	email := "test@example.com"
+	db.Create(&types.User{Name: "DJ", Email: &email})
+	log.Printf("Added User %s %s to the DB\n", "DJ", email)
+
 	server := api.NewAPIServer(":8080")
 	server.Run()
 }
