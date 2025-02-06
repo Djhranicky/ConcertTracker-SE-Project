e3be97ea7357d1f388baa62bf97d1e36d766826d
David Hranicky <djh30000@gmail.com>
2025-02-06 11:41:05 -0500

    Add tests for failing cases on handleRegister

--- backend/routes/routes_test.go
+++ backend/routes/routes_test.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,6 +17,24 @@ import (
 func TestUserServiceHandlers(t *testing.T) {
 	userStore := &MockUserStore{}
 	handler := NewHandler(userStore)
+
+	t.Run("Should fail if request body is empty", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodPost, "/register", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.handleRegister)
+
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %v, got status code %v", http.StatusBadRequest, rr.Code)
+		}
+	})
 
 	t.Run("Should fail if the user payload is invalid", func(t *testing.T) {
 		payload := types.UserRegisterPayload{
@@ -36,6 +55,33 @@ func TestUserServiceHandlers(t *testing.T) {
 		router.HandleFunc("/register", handler.handleRegister)
 
 		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %v, got status code %v", http.StatusBadRequest, rr.Code)
+		}
+	})
+
+	t.Run("Should fail if user exists", func(t *testing.T) {
+		payload := types.UserRegisterPayload{
+			Name:     "DJ Hranicky",
+			Email:    "test@example3.com",
+			Password: "testpw",
+		}
+		marshalled, _ := json.Marshal(payload)
+
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.handleRegister)
+
+		router.ServeHTTP(rr, req)
+
+		log.Print(rr.Body)
 
 		if rr.Code != http.StatusBadRequest {
 			t.Errorf("expected status code %v, got status code %v", http.StatusBadRequest, rr.Code)
