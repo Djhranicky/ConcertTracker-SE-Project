bff20d77bbc76f03d7da40cb3db5989a3598d69e
David Hranicky <djh30000@gmail.com>
2025-02-19 22:08:44 -0500

    Add getJWT util function

--- backend/service/auth/session.go
+++ backend/service/auth/session.go
@@ -1,6 +1,12 @@
 package auth
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/djhranicky/ConcertTracker-SE-Project/utils"
+)
 
 func setJWTCookie(w http.ResponseWriter, token string) {
 	cookie := http.Cookie{
@@ -10,4 +16,18 @@ func setJWTCookie(w http.ResponseWriter, token string) {
 	}
 
 	http.SetCookie(w, &cookie)
+}
+
+func getJWTCookie(w http.ResponseWriter, r http.Request) *http.Cookie {
+	cookie, err := r.Cookie("id")
+	if err != nil {
+		switch {
+		case errors.Is(err, http.ErrNoCookie):
+			utils.WriteError(w, http.StatusBadRequest, err)
+		default:
+			utils.WriteError(w, http.StatusInternalServerError, fmt.Errorf("Server error getting JWT Cookie"))
+		}
+		return nil
+	}
+	return cookie
 }
