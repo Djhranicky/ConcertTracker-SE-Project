e7ee3b860f2e5cc3a4c98f3f61f757c76ea5d99b
David Hranicky <djh30000@gmail.com>
2025-02-23 10:58:09 -0500

    Update jwt token to use default name for expiration

--- backend/service/auth/jwt.go
+++ backend/service/auth/jwt.go
@@ -13,8 +13,8 @@ func CreateJWT(secret []byte, userID uint) (string, error) {
 	expiration := time.Second * time.Duration(3600*24*31)
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"userID":    strconv.Itoa(int(userID)),
-		"expiredAt": time.Now().Add(expiration).Unix(),
+		"userID": strconv.Itoa(int(userID)),
+		"exp":    time.Now().Add(expiration).Unix(),
 	})
 
 	tokenString, err := token.SignedString(secret)
