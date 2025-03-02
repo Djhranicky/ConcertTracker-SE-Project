fa377906499dbef1b04a78ab5f025e56be4fdef0
paolagabriellas <paola.solari@ufl.edu>
2025-03-01 21:21:32 -0500

    Basic unit testing for landing page, its components and services #44

--- frontend/src/app/pages/signup/signup.component.spec.ts
+++ frontend/src/app/pages/signup/signup.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { SignupComponent } from './signup.component';
 
@@ -8,16 +11,20 @@ describe('SignupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignupComponent]
-    })
-    .compileComponents();
+      imports: [SignupComponent],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
