7bd0d714baf308794b9481734341309b86136217
David Hranicky <djh30000@gmail.com>
2025-03-23 19:20:56 -0400

    Add basic ProcessArtistInfo function

--- backend/service/setlist/setlist.go
+++ backend/service/setlist/setlist.go
@@ -61,3 +61,51 @@ func ArtistSearch(url string, artist string) (*types.Artist, error) {
 	}
 	return &returnArtist, nil
 }
+
+func ProcessArtistInfo(store types.Store) {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		return
+	}
+
+	xAPIKey := []byte(os.Getenv("SETLIST_API_KEY"))
+
+	req, err := http.NewRequest("GET", "https://api.setlist.fm/rest/1.0/artist/f4abc0b5-3f7a-4eff-8f78-ac078dbce533/setlists", nil)
+	if err != nil {
+		return
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("x-api-key", string(xAPIKey))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	var jsonData Artist_MBID_Setlists
+	err = json.Unmarshal(body, &jsonData)
+	if err != nil {
+		return
+	}
+
+	for i := 0; i < jsonData.ItemsPerPage; i++ {
+		current := jsonData.Setlist[i]
+		store.CreateVenueIfMissing(types.Venue{
+			Name:    current.Venue.Name,
+			City:    current.Venue.City.Name,
+			Country: current.Venue.City.Country.Name,
+		})
+	}
+}
