4276ada66739ef486cac1a51bfdd88af500a71d3
David Hranicky <djh30000@gmail.com>
2025-03-26 19:35:57 -0400

    Add song type definition for DB

--- backend/types/types.go
+++ backend/types/types.go
@@ -78,3 +78,13 @@ type Concert struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+type Song struct {
+	ID     uint   `gorm:"primaryKey"`
+	Name   string `json:"name" gorm:"uniqueIndex:composite"`
+	Info   string `json:"info"`
+	Tape   bool   `json:"tape"`
+	Artist Artist `gorm:"foreignKey:ArtistID;uniqueIndex:composite"`
+	With   Artist `gorm:"foreignKey:ArtistID"`
+	Cover  Artist `gorm:"foreignKey:ArtistID"`
+}
