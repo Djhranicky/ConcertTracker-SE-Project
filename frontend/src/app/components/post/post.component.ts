d50273320c666252c37752106b7a23369ca8afc7
srijareddyk <srijareddy2803@gmail.com>
2025-03-30 18:22:41 -0400

    Updated Post Component to Show "You"
    Modified the Post Component to check if the post's username matches the current user
    
    Added the getDisplayUsername method to display "You" instead of the username when appropriate
    
    Updated the HTML template to use this method

--- frontend/src/app/components/post/post.component.ts
+++ frontend/src/app/components/post/post.component.ts
@@ -1,22 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostService, Post } from '../../services/post.service';
+import { UserService } from '../../services/user.service';
 import { Button } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { Avatar } from 'primeng/avatar';
 import { TimeAgoPipe } from '../../utils/time-ago.pipe';
+
 @Component({
   selector: 'app-post',
   imports: [CardModule, Button, Avatar, CommonModule, TimeAgoPipe],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css',
-  providers: [PostService],
+  providers: [PostService, UserService],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   isLiked: boolean = false;
+  isCurrentUser: boolean = false;
+  currentUsername: string = '';
 
   @Input() post: Post;
-  // responsiveOptions: any[] | undefined;
+  
+  constructor(private userService: UserService) {}
+  
+  ngOnInit(): void {
+    // Check if the post is from the current user
+    this.userService.getUserProfile().subscribe(profile => {
+      this.currentUsername = profile.name;
+      this.isCurrentUser = this.post.username === this.currentUsername;
+    });
+  }
 
   getStars(rating: number): number[] {
     return Array(rating).fill(0);
@@ -29,5 +42,9 @@ export class PostComponent {
     } else {
       this.post.likes = this.post.likes - 1;
     }
+  }
+  
+  getDisplayUsername(): string {
+    return this.isCurrentUser ? 'You' : this.post.username;
   }
 }
