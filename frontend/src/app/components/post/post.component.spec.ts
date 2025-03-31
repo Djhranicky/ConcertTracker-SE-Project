de69907b6d52a78d2e68ac69e1ff17520c48a43f
paolagabriellas <paola.solari@ufl.edu>
2025-03-31 17:07:32 -0400

    fix post test

--- frontend/src/app/components/post/post.component.spec.ts
+++ frontend/src/app/components/post/post.component.spec.ts
@@ -1,7 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { of } from 'rxjs';
 import { PostComponent } from './post.component';
+import { Post } from '../../services/post.service';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 describe('PostComponent', () => {
   let component: PostComponent;
   let fixture: ComponentFixture<PostComponent>;
@@ -9,18 +17,47 @@ describe('PostComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PostComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+      teardown: { destroyAfterEach: false },
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(PostComponent);
     component = fixture.componentInstance;
+    component.post = {
+      type: 'review',
+      username: 'John Doe',
+      avatar: 'imgurl',
+      postDate: 'Feb 20, 2025',
+      date: 'Feb 19, 2025',
+      venue: 'Brisbane Entertainment Centre, Brisbane, Australia',
+      artist: 'Billie Eilish',
+      setlist: null,
+      tour: 'HIT ME HARD AND SOFT',
+      img: 'https://res.cloudinary.com/hits-photos-archive/image/upload/v1736890770/legacy-migration/legacy-hitsdd_photo_gal__photo_1891402125.png',
+      reviewText: null,
+      attachedImg: null,
+      rating: 4,
+      likes: 10,
+      comments: 3,
+    };
     fixture.detectChanges();
   });
-  const mockService = jasmine.createSpyObj('mockService', ['getPosts']);
-  mockService.getPosts.and.returnValue(of(true));
+  // const mockService = jasmine.createSpyObj('mockService', ['getPosts']);
+  // mockService.getPosts.and.returnValue(of(true));
   it('should create', () => {
-    expect(true).toBeTruthy();
-    expect(mockService.getPosts);
+    expect(component).toBeTruthy();
   });
 
-  //post should have
+  //toggleLike()
+  it('should toggle isLiked affect post like count', () => {
+    component.toggleLike();
+    expect(component.isLiked).toBeTrue();
+    expect(component.post.likes).toBe(11);
+
+    component.toggleLike();
+    expect(component.isLiked).toBeFalse();
+    expect(component.post.likes).toBe(10);
+  });
 });
