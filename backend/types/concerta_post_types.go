938be66d02477a063233d05ed48e0e6430010d4e
David Hranicky <djh30000@gmail.com>
2025-04-09 16:13:13 -0400

    Add created and updated firlds to post tables

--- backend/types/concerta_post_types.go
+++ backend/types/concerta_post_types.go
@@ -1,17 +1,23 @@
 package types
 
+import "time"
+
 type Post struct {
-	ID       uint   `gorm:"primaryKey"`
-	AuthorID uint   `gorm:"index"`
-	Text     string `json:"text"`
+	ID        uint   `gorm:"primaryKey"`
+	AuthorID  uint   `gorm:"index"`
+	Text      string `json:"text"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 
 	User User `gorm:"foreignKey:AuthorID"`
 }
 
 type Likes struct {
-	ID     uint `gorm:"primaryKey"`
-	PostID uint `gorm:"index"`
-	UserID uint `gorm:"index"`
+	ID        uint `gorm:"primaryKey"`
+	PostID    uint `gorm:"index"`
+	UserID    uint `gorm:"index"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 
 	Post Post `gorm:"foreignKey:PostID"`
 	User User `gorm:"foreignKey:UserID"`
