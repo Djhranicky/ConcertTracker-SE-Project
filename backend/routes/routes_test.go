570523d99354638c28e525a4d282c6e7cec1c7c8
David Hranicky <djh30000@gmail.com>
2025-04-15 18:37:28 -0400

    Finish tests for handleUserPost

--- backend/routes/routes_test.go
+++ backend/routes/routes_test.go
@@ -639,23 +639,150 @@ func TestUserServiceHandlePost(t *testing.T) {
 	})
 
 	t.Run("should fail with no 'type' field included in incoming json", func(t *testing.T) {
+		text := "Test"
+		rating := uint(1)
+		userPostID := uint(1)
+		payload := &types.UserPostCreatePayload{
+			AuthorID:   1,
+			Text:       &text,
+			Rating:     &rating,
+			UserPostID: &userPostID,
+			IsPublic:   true,
+			ConcertID:  1,
+		}
+		marshalled, _ := json.Marshal(payload)
 
+		req, err := http.NewRequest(http.MethodPost, "/userpost", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/userpost", handler.handleUserPost())
+
+		router.ServeHTTP(rr, req)
+
+		assertEqual(t, http.StatusBadRequest, rr.Code)
 	})
 
 	t.Run("should fail with no 'isPublic' field included in incoming json", func(t *testing.T) {
+		text := "Test"
+		rating := uint(1)
+		userPostID := uint(1)
+		payload := &types.UserPostCreatePayload{
+			AuthorID:   1,
+			Text:       &text,
+			Type:       "ATTENDED",
+			Rating:     &rating,
+			UserPostID: &userPostID,
+			ConcertID:  1,
+		}
+		marshalled, _ := json.Marshal(payload)
 
+		req, err := http.NewRequest(http.MethodPost, "/userpost", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/userpost", handler.handleUserPost())
+
+		router.ServeHTTP(rr, req)
+
+		assertEqual(t, http.StatusBadRequest, rr.Code)
 	})
 
 	t.Run("should fail with no 'concertID' field included in incoming json", func(t *testing.T) {
+		text := "Test"
+		rating := uint(1)
+		userPostID := uint(1)
+		payload := &types.UserPostCreatePayload{
+			AuthorID:   1,
+			Text:       &text,
+			Type:       "ATTENDED",
+			Rating:     &rating,
+			UserPostID: &userPostID,
+			IsPublic:   true,
+		}
+		marshalled, _ := json.Marshal(payload)
 
+		req, err := http.NewRequest(http.MethodPost, "/userpost", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/userpost", handler.handleUserPost())
+
+		router.ServeHTTP(rr, req)
+
+		assertEqual(t, http.StatusBadRequest, rr.Code)
 	})
 
 	t.Run("should fail if invalid 'type' supplied", func(t *testing.T) {
+		text := "Test"
+		rating := uint(1)
+		userPostID := uint(1)
+		payload := &types.UserPostCreatePayload{
+			AuthorID:   1,
+			Text:       &text,
+			Type:       "WRONG_TYPE",
+			Rating:     &rating,
+			UserPostID: &userPostID,
+			IsPublic:   true,
+			ConcertID:  1,
+		}
+		marshalled, _ := json.Marshal(payload)
 
+		req, err := http.NewRequest(http.MethodPost, "/userpost", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/userpost", handler.handleUserPost())
+
+		router.ServeHTTP(rr, req)
+
+		assertEqual(t, http.StatusBadRequest, rr.Code)
 	})
 
 	t.Run("should pass and create post in database", func(t *testing.T) {
+		text := "Test"
+		rating := uint(1)
+		userPostID := uint(1)
+		payload := &types.UserPostCreatePayload{
+			AuthorID:   1,
+			Text:       &text,
+			Type:       "ATTENDED",
+			Rating:     &rating,
+			UserPostID: &userPostID,
+			IsPublic:   true,
+			ConcertID:  1,
+		}
+		marshalled, _ := json.Marshal(payload)
 
+		req, err := http.NewRequest(http.MethodPost, "/userpost", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/userpost", handler.handleUserPost())
+
+		router.ServeHTTP(rr, req)
+
+		assertEqual(t, http.StatusCreated, rr.Code)
 	})
 }
 
